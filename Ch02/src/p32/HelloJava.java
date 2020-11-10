784baed46da56570519de9ed1532fe39e1b2af61
dbwls5573 <dbwndbwn5@gmail.com>
2020-11-10 12:19:57 +0900

    2020.11.10 Ch02 등

---
+++ Ch02/src/p32/HelloJava.java
@@ -0,0 +1,9 @@
+package p32;
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 김철학
+ * 내용 : 교재 p32 따라하기
+ */
+public class HelloJava {
+
+}
---
+++ Ch02/src/sub1/HelloJava.java
@@ -0,0 +1,13 @@
+package sub1;
+
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 김철학
+ * 내용 : Ch02. 자바설치 교재 p31
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
